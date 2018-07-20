208c66cbd0ddb995fd6caff457e94f067a05858e
Valentina <valentina.cecchini.94@gmail.com>
2018-07-20 17:32:37 +0200

    Add References (link of documents) and "our" LUA code

---
+++ scene/Scene_main_script.lua
@@ -0,0 +1,15 @@
+--[[
+    The following function was added in the main script of the scene: 
+    it create the package that will be load on one of the robot.
+--]]
+
+spawnCube=function(inInts,inFloats,inStrings,inBuffer)
+	-- inInts, inFloats and inStrings are tables
+	-- inBuffer is a string
+    
+    -- Perform any type of operation here.
+    local handle = sim.createPureShape(0,8,{0.15,0.15,0.15},3,nil)
+
+	-- Always return 3 tables and a string, e.g.:
+	return {handle},{},{},''
+end
